7b97fb70811a3c25a5ee705962b07be517db644a
Hoddie54 <hoddie54@gmail.com>
2020-11-16 01:05:37 +0000

    Small error

--- PathwaysProject/settings.py
+++ PathwaysProject/settings.py
@@ -141,7 +141,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
