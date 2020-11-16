049d96940918c5a751cdcff583ebcc18d61757ad
Hoddie54 <hoddie54@gmail.com>
2020-11-16 01:07:39 +0000

    Added almost all question functionality

--- PathwaysProject/settings.py
+++ PathwaysProject/settings.py
@@ -142,7 +142,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
 
 
 
